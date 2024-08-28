04aa791034a9fef5468db1fca0c02125fae4bb34
Andrea <andrea_pozas_24@outlook.com>
2024-08-28 17:51:39 -0600

    Se agregaron los archivos Locators.py y Test.py. En Locators se encuentran los localizadores y métodos y en Test se encuentran las pruebas de Urban Routes

---
+++ Locators.py
@@ -0,0 +1,202 @@
+import main
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+# Localizadores para las pruebas
+class UrbanRoutesPage:
+    from_field = (By.ID, 'from')
+    to_field = (By.ID, 'to')
+    button_get_taxi = (By.CSS_SELECTOR, ".button.round")
+    comfort_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[1]/div[5]/div[1]/img')
+    active_ride_comfort = (By.XPATH, '//button[@data-for="tariff-card-4"]')
+    number_field = (By.CSS_SELECTOR, ".np-button")
+    click_number_input = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[1]/div[1]/label')
+    add_phone = (By.ID, "phone")
+    next_button = (By.XPATH, '//*[text()="Siguiente"]')
+    code_field = (By.XPATH, '//label[@for="code"]')
+    add_code = (By.ID, "code")
+    button_confirm = (By.XPATH, '//*[text()="Confirmar"]')
+    number_text = (By.CSS_SELECTOR, '.np-text')
+    payment_method_field = (By.CSS_SELECTOR, ".pp-text")
+    button_plus_card = (By.CLASS_NAME, "pp-plus-container")
+    add_number_card = (By.ID, "number")
+    add_code_card = (By.NAME, "code")
+    rectangle_colors = (By.CSS_SELECTOR, ".plc")
+    button_add_card_code = (By.XPATH, '//*[text()="Agregar"]')
+    button_close_payment_method = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[1]/button')
+    card_image = (By.CSS_SELECTOR, ".pp-value-container")
+    select_message_driver = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[3]/div/label')
+    message_driver_field = (By.ID, "comment")
+    select_blanket_and_tissue = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span')
+    confirm_blanket_and_tissue = (By.CSS_SELECTOR,'#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.form > div.reqs.open > div.reqs-body > div:nth-child(1) > div > div.r-sw > div > input')
+    add_ice_cream = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]')
+    select_two_ice_creams = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[2]')
+    search_taxi_button = (By.CSS_SELECTOR, ".smart-button-main")
+    search_car_screen = (By.CSS_SELECTOR, ".order-header-title")
+    img_driver = (By.XPATH, '//img[@src="/static/media/bender.e90e5089.svg"]')
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # 1. Configuración de las dirrecciones
+    def set_from(self, from_address):
+        self.driver.find_element(*self.from_field).send_keys(from_address)
+
+    def set_to(self, to_address):
+        self.driver.find_element(*self.to_field).send_keys(to_address)
+
+    def get_from(self):
+        return self.driver.find_element(*self.from_field).get_property('value')
+
+    def get_to(self):
+        return self.driver.find_element(*self.to_field).get_property('value')
+
+    def set_route(self, from_adress, to_adress):
+        self.set_from(from_adress)
+        self.set_to(to_adress)
+
+    # 2.Seleccionar la tarifa Comfort
+    def click_button_get_taxi(self):
+        self.driver.find_element(*self.button_get_taxi).click()
+
+    def click_ride_comfort(self):
+        self.driver.find_element(*self.comfort_button).click()
+
+    def active_comfort(self):
+        return self.driver.find_element(*self.active_ride_comfort).is_displayed()
+
+    def select_ride_comfort(self):
+        self.click_button_get_taxi()
+        self.click_ride_comfort()
+        self.active_comfort()
+
+    # 3.Seleccionar y rellenar el campo número de telefono
+    def click_phone_field(self):
+        self.driver.find_element(*self.number_field).click()
+
+    def click_add_phone_field(self):
+        self.driver.find_element(*self.click_number_input).click()
+
+    def add_phone_number(self, number):
+        self.driver.find_element(*self.add_phone).send_keys(number)
+
+    def click_next_button(self):
+        self.driver.find_element(*self.next_button).click()
+
+    def add_code_field(self):
+        self.driver.find_element(*self.add_code).send_keys(main.retrieve_phone_code(self.driver))
+
+    def click_button_confirm(self):
+        self.driver.find_element(*self.button_confirm).click()
+
+    def write_phone_number_correct(self):
+        return self.driver.find_element(*self.number_text).text
+
+    def add_number_phone_field(self, number):
+        self.click_phone_field()
+        self.click_add_phone_field()
+        self.add_phone_number(number)
+        self.click_next_button()
+        self.add_code_field()
+        self.click_button_confirm()
+
+    # 4.Agregar una tarjeta de crédito
+    def click_payment_method_field(self):
+        self.driver.find_element(*self.payment_method_field).click()
+
+    def click_plus_button_card(self):
+        self.driver.find_element(*self.button_plus_card).click()
+
+    def add_card_number(self, card_number):
+        self.driver.find_element(*self.add_number_card).send_keys(card_number)
+
+    def add_card_code(self, card_code):
+        self.driver.find_element(*self.add_code_card).send_keys(card_code)
+
+    def click_another_place_screen(self):
+        self.driver.find_element(*self.rectangle_colors).click()
+
+    def click_add_card_button(self):
+        self.driver.find_element(*self.button_add_card_code).click()
+
+    def click_close_payment_method_screen(self):
+        self.driver.find_element(*self.button_close_payment_method).click()
+
+    def correct_add_card(self):
+        return self.driver.find_element(*self.card_image).is_displayed()
+
+    def add_card_payment_method(self, card_number, card_code):
+        self.click_payment_method_field()
+        self.click_plus_button_card()
+        self.add_card_number(card_number)
+        self.add_card_code(card_code)
+        self.click_another_place_screen()
+        self.click_add_card_button()
+        self.click_close_payment_method_screen()
+
+    # 5.Mensaje para el conductor
+    def click_message_driver(self):
+        self.driver.find_element(*self.select_message_driver).click()
+
+    def write_message_driver(self, message):
+        self.driver.find_element(*self.message_driver_field).send_keys(message)
+
+    def get_message_for_driver(self):
+        return self.driver.find_element(*self.message_driver_field).get_property('value')
+
+    def send_message_for_driver(self, message):
+        self.click_message_driver()
+        self.write_message_driver(message)
+
+    # 6. Pedir manta y pañuelos
+    def click_select_blanket_tissue(self):
+        self.driver.find_element(*self.select_blanket_and_tissue).click()
+
+    def get_blanket_tissue(self):
+        return self.driver.find_element(*self.confirm_blanket_and_tissue).is_selected()
+
+    # 7.Seleccionar dos helados
+    def click_add_first_ice_cream(self):
+        self.driver.find_element(*self.add_ice_cream).click()
+
+    def click_add_second_ice_cream(self):
+        self.driver.find_element(*self.add_ice_cream).click()
+
+    def set_two_ice_creams(self):
+        return self.driver.find_element(*self.select_two_ice_creams).text
+
+    def add_two_ice_creams(self):
+        self.click_add_first_ice_cream()
+        self.click_add_second_ice_cream()
+
+    #  8. Seleccionar el botón para buscar un taxi
+    def click_search_taxi_button(self):
+        self.driver.find_element(*self.search_taxi_button).click()
+
+    def open_search_taxi_screen(self):
+        return self.driver.find_element(*self.search_car_screen).is_displayed()
+
+    # 9. Esperar a que aparezca la información del conductor en el modal
+    def open_information_driver_screen(self):
+        WebDriverWait(self.driver, 300).until(expected_conditions.visibility_of_element_located(self.img_driver))
+        return self.driver.find_element(*self.img_driver).is_displayed()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+PRUEBAS AUTOMATIZADAS PARA PEDIR UN TAXI
+
+Pruebas automatizadas UI para para curbrir el proceso de pedir un taxi en la aplicación web Uraban Routes
+
+Pruebas para pedir un taxi de la tarifa comfort de Urban Routes
+
+Para realizar los test se necesitan tener instalados los paquetes Selenium y pytest
+
+Las pruebas que se ejecutan son las siguientes:
+test_set_route: verifica que las direcciones han sido agregadadas correctamente 
+test_select_comfort: verifica que la tarifa conform ha sido seleccionada 
+test_phone_number_correct: verifica que el número de teléfono se haya agregado correctamente 
+test_add_card: verifica que la tajeta se ha agregado 
+test_message_for_driver: verifica que el mensaje para el conductor se agrego correctamente
+test_button_blanket_and_tissue: verifica que el botón "Manta y pañuelos" esté seleccionado
+test_add_two_ice_creams: verifica que se han agregado 2 helados 
+test_select_search_taxi_button: verifica que se muestra el modal de búsqueda de taxi
+test_driver_information_screen : verifica que se muestra el modal con la información del conductor 
+
+Para ejecutar todas las pruebas se puede utilizar el comando: pytest Test.py
+
+Andrea Lizbeth Pozas Rivera, cohort 12, sprint 8 
